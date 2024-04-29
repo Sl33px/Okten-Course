f8c7c36fe706aedad0ea0d23e1024391dc6e8ca6
Sl33px <shaposhnikov5580@gmail.com>
2024-04-29 22:12:45 +0300

    add hw1

---
+++ JS & TS/Homework/script.js
@@ -0,0 +1,49 @@
+let word1 = 'hello'
+let word2 = 'owu'
+let word3 = 'com'
+let word4 = 'ua'
+let num1 = 1
+let num2 = 10
+let num3 = -999
+let num4 = 123
+let num5 = 3.14
+let num6 = 2.7
+let num7 = 16
+let bool1 = true
+let bool2 = false
+
+let firstName = 'Dmytro'
+let middleName = 'Illich'
+let lastName = 'Shaposhnikov'
+
+let person = `${lastName} ${firstName} ${middleName}`
+
+console.log(word1)
+console.log(word2)
+console.log(word3)
+console.log(word4)
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(num4)
+console.log(num5)
+console.log(num6)
+console.log(num7)
+console.log(bool1)
+console.log(bool2)
+console.log(person)
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let prompt1 = prompt('What is your firstname?');
+let prompt2 = prompt('What is your middlename?');
+let prompt3 = prompt('What is your age');
+console.log(`firstname: ${prompt1}`);
+console.log(`middlename: ${prompt2}`);
+console.log(`age: ${prompt3}`);
